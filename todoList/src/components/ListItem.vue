f994aa54094da3454e7aa18b98e85f0112834c33
OmarT1502 <l21390389@chetumal.tecnm.mx>
2025-05-30 19:13:49 -0500

    todo list

---
+++ todoList/src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <label :class="{ checked: isChecked }">
+    <input type="checkbox" :checked="isChecked" />
+    <slot></slot>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+.checked {
+  text-decoration: line-through;
+}
+</style>
+
+<script lang="ts" setup>
+defineProps<{
+  isChecked?: boolean | false
+}>()
+</script>
